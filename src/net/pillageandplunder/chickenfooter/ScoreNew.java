936c697af5e9c6a5bf1c48179ee06fdaeb56c91e
Jeremy Stephens <viking415@gmail.com>
2009-01-22 10:33:19 -0600

    Made ScoreNew finish on receiving Enter

--- src/net/pillageandplunder/chickenfooter/ScoreNew.java
+++ src/net/pillageandplunder/chickenfooter/ScoreNew.java
@@ -3,8 +3,8 @@ package net.pillageandplunder.chickenfooter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class ScoreNew extends Activity {
@@ -21,14 +21,16 @@ public class ScoreNew extends Activity {
         }
 
 		mValueText = (EditText) findViewById(R.id.new_score_value);
-		Button confirmButton = (Button) findViewById(R.id.new_score_confirm);
+		mValueText.setOnKeyListener(new View.OnKeyListener() {
+			public boolean onKey(View view, int keyCode, KeyEvent event) {
+				if (keyCode != KeyEvent.KEYCODE_ENTER)
+					return false;
 
-		confirmButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View view) {
 				Intent mIntent = new Intent();
 				mIntent.putExtra("value", mValueText.getText().toString());
 				setResult(RESULT_OK, mIntent);
 				finish();
+				return true;
 			}
 		});
 	}
